7754ed3b583b7f9dba04b2cd1a23d34846ea37b1
Wojtek Zieziula <wojciech.zieziula@gmail.com>
2024-12-30 21:05:19 +0100

    added get_shape() method

--- game_logic/board.py
+++ game_logic/board.py
@@ -10,3 +10,17 @@ class Board:
             raise InvalidArgumentException("rows/columns amount has to be a positive number!")
         self.rows = rows
         self.columns = columns
+
+    def get_shape(self):
+        return self.rows, self.columns
+
+#       0   1   2   3   4
+# 0     X - X - X - X - X
+#       | \ | / | \ | / |
+# 1     X - X - X - X - X
+#       | / | \ | / | \ |
+# 2     X - X - X - X - X
+
+# (0, 0): [(0, 1), (1, 0), (1, 1)]
+# (0, 1): [(0, 0), (1, 1), (0, 2)]
+# (0, 2): []
